4e21e7312e8331e31b0fe28d020fb04e95315404
MuresanIA <muresan.alexandru933@gmail.com>
2021-01-10 16:18:47 +0200

    Working #9

--- src/com/MuresanIA/TicTacToe.java
+++ src/com/MuresanIA/TicTacToe.java
@@ -1,5 +1,7 @@
 package com.MuresanIA;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,18 +16,36 @@ public class TicTacToe {
 
         printGameBoard(gameBoard);
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your placement (1-9):");
-        int playerPos = scan.nextInt();
 
-        placePiece(gameBoard, playerPos, "player");
+        while (true) {
+            Scanner scan = new Scanner(System.in);
 
-        Random rand = new Random();
-        int cpuPos = rand.nextInt(9) + 1;
-        placePiece(gameBoard, cpuPos, "cpu");
+            System.out.println("Enter your placement (1-9):");
+            int playerPos = scan.nextInt();
 
-        printGameBoard(gameBoard);
+            placePiece(gameBoard, playerPos, "player");
 
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) + 1;
+            placePiece(gameBoard, cpuPos, "cpu");
+
+            printGameBoard(gameBoard);
+
+        }
+    }
+
+    public static String checkWinner() {
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        return "";
     }
 
     public static void printGameBoard(char[][] gameBoard) {
