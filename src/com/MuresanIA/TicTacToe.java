d6e8d9523aa378d6cf2eb51e99cb31c02721ba77
MuresanIA <muresan.alexandru933@gmail.com>
2021-01-10 15:52:35 +0200

    Working #4

--- src/com/MuresanIA/TicTacToe.java
+++ src/com/MuresanIA/TicTacToe.java
@@ -3,14 +3,19 @@ package com.MuresanIA;
 public class TicTacToe {
 
     public static void main(String[] args) {
-	char[][] gameBoard ={{' ', '|', ' ', '|', ' '},
-            {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '},
-            {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '}};
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
 
-	    for (char[] row : gameBoard){
-	        for (char c: row){
+        printGameBoard(gameBoard);
+
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
                 System.out.print(c);
             }
             System.out.println();
