fe0f79c2067d5d050d970ad43780074cf6341c42
MuresanIA <muresan.alexandru933@gmail.com>
2021-01-10 16:34:44 +0200

    Working #12

--- src/com/MuresanIA/TicTacToe.java
+++ src/com/MuresanIA/TicTacToe.java
@@ -23,10 +23,22 @@ public class TicTacToe {
             System.out.println("Enter your placement (1-9):");
             int playerPos = scan.nextInt();
 
+            while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPositions)) {
+                System.out.println("Position taken! Enter a correct position!");
+                playerPos = scan.nextInt();
+            }
+
+
             placePiece(gameBoard, playerPos, "player");
 
             Random rand = new Random();
             int cpuPos = rand.nextInt(9) + 1;
+
+            while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+                System.out.println("Position taken! Enter a correct position!");
+                cpuPos = rand.nextInt(9) + 1;
+            }
+
             placePiece(gameBoard, cpuPos, "cpu");
 
             printGameBoard(gameBoard);
@@ -60,9 +72,9 @@ public class TicTacToe {
         for (List l : winning) {
             if (playerPositions.containsAll(l)) {
                 return "Congratulations you won!";
-            }else if (cpuPositions.containsAll(l)){
+            } else if (cpuPositions.containsAll(l)) {
                 return "CPU wins!";
-            }else if (playerPositions.size() + cpuPositions.size() == 9){
+            } else if (playerPositions.size() + cpuPositions.size() == 9) {
                 return "CAT!";
             }
         }
